6113fe06cf9e51e7e9ece66563b27cace2c713d0
John Doe <patrick.gould94@gmail.com>
2018-12-21 18:28:56 -0500

    added modal & match logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,8 +14,96 @@ module.exports =
         5,
         4,
         1
-        ]
-  }]
+        ],
+    "difference": 0
+  },
+  {
+    "name":"Ryan",
+    "photo":"http://www.gstatic.com/tv/thumb/persons/157012/157012_v9_ba.jpg",
+    "scores":[
+        3,
+        2,
+        5,
+        5,
+        2,
+        1,
+        5,
+        2,
+        3,
+        1  
+    ],
+    "difference": 0},
+  {
+    "name":"George",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/George_Clooney_2016.jpg/220px-George_Clooney_2016.jpg",
+    "scores":[
+        1,
+        5,
+        3,
+        2,
+        1,
+        4,
+        5,
+        5,
+        1,
+        2
+  ],
+    "difference": 0
+
+  },
+  {
+    "name":"Brad",
+    "photo":"https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+    "scores":[
+        1,
+        2,
+        3,
+        3,
+        4,
+        5,
+        3,
+        1,
+        2,
+        3
+  ],
+  "difference": 0},
+  {
+    "name":"Ben",
+    "photo":"http://specials-images.forbesimg.com/imageserve/577680124bbe6f5634814d94/416x416.jpg?background=000000&cropX1=108&cropX2=744&cropY1=106&cropY2=743",
+    "scores":[
+        2,
+        3,
+        5,
+        1,
+        2,
+        3,
+        4,
+        1,
+        2,
+        5
+    ],
+    "difference": 0
+  },
+    {
+      "name": "Matt",
+      "photo": "https://m.media-amazon.com/images/M/MV5BMTM0NzYzNDgxMl5BMl5BanBnXkFtZTcwMDg2MTMyMw@@._V1_UY317_CR11,0,214,317_AL_.jpg",
+      "scores": [
+        4,
+        2,
+        3,
+        5,
+        2,
+        3,
+        1,
+        5,
+        2,
+        5
+
+      ],
+      "difference": 0
+    }
+    
+  ]
 
     
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,33 @@ module.exports = function(app){
   }); 
 
   app.post("/api/friends", function(req, res) {
-    // compatibility logic
-    console.log(req.body)
-    // for (let i = 0; i < friends.length; i++) {
-      
-    // }
-    res.json(req.body);
+    
+    // converts scores to numbers
+    let user = req.body;
+    user.scores.map((score, index) =>{
+      user.scores[index] = parseInt(score);
+    });
+   
+    let bestDifference = 100;
+    let bestMatch;
+    
+    // Loop for calculating differences
+    for (let i = 0; i < friends.length; i++) {
+      friends[i].difference = 0;
+      for (let x = 0; x < friends[i].scores.length; x++) {
+            difference = Math.abs(friends[i].scores[x] - user.scores[x])
+            friends[i].difference += difference;
+          }
+    }
+
+    // Loop for calculating the best match
+    for (let y = 0; y < friends.length; y++) {
+      if (friends[y].difference < bestDifference) {
+        bestDifference = friends[y].difference;
+        bestMatch = friends[y]
+      }
+    }
+    res.json(bestMatch);
   });
       
 }
