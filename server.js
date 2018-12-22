ae607c4bdd93aa14b9972649ec90cd1969f925fa
John Doe <patrick.gould94@gmail.com>
2018-12-22 01:24:23 -0500

    added comments and cleaned up code

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Imports all the http routes
 const routes = require("./app/routing/router");
 
+// middleware converting data sent through express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// api(app);
-
-// html(app, path);
-
-
-
-
+// enables all the routes that are defined in router.js
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
